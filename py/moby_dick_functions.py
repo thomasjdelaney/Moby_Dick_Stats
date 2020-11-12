1c495b8389f3e27ae6b933557fd969d29b8c0187
Thomas James Delaney <thomas.delaney@posteo.net>
2020-11-12 19:38:26 +0000

    Added more functions. Fixed some errors in the text. Currently cleaning up text for word counting.

--- py/moby_dick_functions.py
+++ py/moby_dick_functions.py
@@ -44,3 +44,56 @@ def getAllSectionTitles(moby_dick_text):
     num_to_chap_title = extractChapterTitleDict(moby_dick_text)
     all_section_titles = ['CONTENTS', 'ETYMOLOGY', 'EXTRACTS'] + list(num_to_chap_title.values()) + ['Epilogue']
     return all_section_titles
+
+def getSpecialSectionText(moby_dick_text, section_title):
+    """
+    For getting the text of a given special section.
+    Arguments:  moby_dick_text, the text
+                section_title, the title of the sections
+    Returns:    the text of the section
+    """
+    title_to_end_phrase = {'CONTENTS':'Original Transcriber', 'ETYMOLOGY':'EXTRACTS. (Supplied by a', 
+            'EXTRACTS':'CHAPTER 1. Loomings. Call me Ishmael.', 'Epilogue':'End of Project Gutenberg'}
+    if not section_title in list(title_to_end_phrase.keys()):
+        print(dt.datetime.now().isoformat() + ' ERROR: ' + 'Unrecognised special section title! Exiting.')
+        exit(1)
+    title_mention_inds = findMultiple(moby_dick_text, section_title)
+    section_start = title_mention_inds[len(title_mention_inds) > 1]
+    section_end = moby_dick_text.find(title_to_end_phrase.get(section_title))
+    return moby_dick_text[section_start:section_end].strip()
+
+def getChapterText(moby_dick_text, num_to_chap_title, chapter_number_or_name, num_chapters=135):
+    """
+    For geting the text of the given chapter.
+    Arguments:  moby_dick_text, the text
+                num_to_chap_title, dictionary
+                chapter_number_or_name, int, or string
+    Returns:    the text of the given chapter, in a big string
+    """ 
+    special_sections = ['CONTENTS', 'ETYMOLOGY', 'EXTRACTS', 'Epilogue']
+    if int == type(chapter_number_or_name):
+        chapter_number = chapter_number_or_name
+    elif str == type(chapter_number_or_name):
+        if chapter_number_or_name in special_sections:
+            return getSpecialSectionText(moby_dick_text, chapter_number_or_name)
+        chapter_number = list(num_to_chap_title.values()).index(chapter_number_or_name) + 1
+    else:
+        print(dt.datetime.now().isoformat() + ' ERROR: ' + 'Unrecognised chapter identifier type! Exiting!')
+        exit(1)
+    chapter_title = num_to_chap_title.get(chapter_number)
+    next_chapter_title = 'CHAPTER ' + str(chapter_number+1) + '. ' + num_to_chap_title.get(chapter_number+1) if chapter_number < num_chapters else 'Epilogue'
+    chapter_start = findMultiple(moby_dick_text, 'CHAPTER ' + str(chapter_number) + '. ' + chapter_title)[1]
+    chapter_end = findMultiple(moby_dick_text, next_chapter_title)[1]
+    chapter_text = moby_dick_text[chapter_start:chapter_end].strip()
+    return chapter_text
+
+def getNovelText(moby_dick_text):
+    """
+    For getting the text without the parts referring to licensing or the list of contents.
+    Arguments:  moby_dick_text, the text
+    Returns:    the novel text, as described above
+    """
+    novel_start = findMultiple(moby_dick_text, 'ETYMOLOGY')[1]
+    novel_end = moby_dick_text.find('End of Project Gutenberg')
+    return moby_dick_text[novel_start:novel_end].strip()
+
--- py/moby_dick_stats.py
+++ py/moby_dick_stats.py
@@ -14,35 +14,38 @@ with open(moby_dick_txt_file, 'r') as f:
     moby_dick_text = f.read()
 
 num_to_chap_title = extractChapterTitleDict(moby_dick_text)
-
-def getChapterText(moby_dick_text, num_to_chap_title, chapter_number_or_name, num_chapters=135):
-    """
-    For geting the text of the given chapter.
-    Arguments:  moby_dick_text, the text
-                num_to_chap_title, dictionary
-                chapter_number_or_name, int, or string
-    Returns:    the text of the given chapter, in a big string
-    """
-    # TODO Need something here for special chapters 'Extracts, Epilogue, etc'
-    if int == type(chapter_number_or_name):
-        chapter_number = chapter_number_or_name
-    elif str == type(chapter_number_or_name):
-        chapter_number = list(num_to_chap_title.values()).index(chapter_number_or_name) + 1
-    else:
-        print(dt.datetime.now().isoformat() + ' ERROR: ' + 'Unrecognised chapter identifier type! Exiting!')
-        exit(1)
-    chapter_title = num_to_chap_title.get(chapter_number)
-    next_chapter_title = num_to_chap_title.get(chapter_number+1)
-    chapter_start = findMultiple(moby_dick_text, 'CHAPTER ' + str(chapter_number) + '. ' + chapter_title)[1]
-    chapter_end = findMultiple(moby_dick_text, 'CHAPTER ' + str(chapter_number+1) + '. ' + next_chapter_title)[1]
-    # TODO need something to deal with the last chapter.
-    chapter_text = moby_dick_text[chapter_start:chapter_end].strip()
-    return chapter_text
-
 all_section_titles = getAllSectionTitles(moby_dick_text)
+
+# these are unit tests
 loomings_text_1 = getChapterText(moby_dick_text, num_to_chap_title, 1)
 loomings_text_2 = getChapterText(moby_dick_text, num_to_chap_title, 'Loomings')
 chapter_2_text = getChapterText(moby_dick_text, num_to_chap_title, 2)
+chapter_135_text = getChapterText(moby_dick_text, num_to_chap_title, 135)
+contents_text = getSpecialSectionText(moby_dick_text, 'CONTENTS')
+epilogue_text = getSpecialSectionText(moby_dick_text, 'Epilogue')
+novel_text = getNovelText(moby_dick_text)
+
+# try to count how many times each word is used. Need to do some formatting first
+# might need to extract all the proper nouns also
+novel_text = novel_text.replace('“', '')
+novel_text = novel_text.replace('”', '')
+novel_text = novel_text.replace(';', '')
+novel_text = novel_text.replace(',', '')
+novel_text = novel_text.replace('.', '')
+novel_text = novel_text.replace('!', '')
+novel_text = novel_text.replace('?', '')
+novel_text = novel_text.replace(':', '')
+novel_text = novel_text.replace('*', '')
+novel_text = novel_text.replace('(', '')
+novel_text = novel_text.replace(')', '')
+novel_text = novel_text.replace("’s", '')
+novel_text = novel_text.replace("’ ", ' ')
+novel_text = novel_text.replace('—', ' ')
+novel_text = novel_text.replace('-', ' ')
+while novel_text.find('  ') != -1:
+    novel_text = novel_text.replace('  ',  ' ')
+all_words = novel_text.split(' ')
+unique_words, word_counts = np.unique(all_words, return_counts=True)
+
 
 # TODO: Unit test file
-#       Function to trim text of anything not in the story
